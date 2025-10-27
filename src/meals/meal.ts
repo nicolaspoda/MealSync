3cbe011aa840d72ebb46153ef891cad38baaa40e
Michele <michele.florio@ynov.com>
2025-10-27 18:09:33 +0100

    feat(uuid): add a UUID type to use to set UUIDv4 as a type for id properties

--- src/meals/meal.ts
+++ src/meals/meal.ts
@@ -1,5 +1,11 @@
+import { UUID } from "../shared/uuid";
+
+/**
+ * The Meal object contains common information across
+ * every meal in the system regardless of calories and aliments.
+ */
 export interface Meal {
-  id: string;
+  id: UUID;
   title: string;
   description: string;
   calories: number;
